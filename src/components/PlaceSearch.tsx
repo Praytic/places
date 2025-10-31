77533f8c30889433e755d47a81a1c01a01a73341
Praytic <chernogorovvm@gmail.com>
2025-10-31 11:27:36 -0700

    Add 🎃 pumpkin emoji as custom emoji to emoji picker

--- src/components/PlaceSearch.tsx
+++ src/components/PlaceSearch.tsx
@@ -353,6 +353,13 @@ const PlaceSearch: React.FC<PlaceSearchProps> = ({
           previewConfig={{showPreview: false}}
           emojiStyle={EmojiStyle.NATIVE}
           skinTonesDisabled={true}
+          customEmojis={[
+            {
+              names: ['pumpkin', 'halloween', 'trick or treat'],
+              imgUrl: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36"><text y="32" font-size="32">🎃</text></svg>',
+              id: 'pumpkin'
+            }
+          ]}
         />
       </Dialog>
     </>
